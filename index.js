6167452b232c0c3bed8891872a65b728aeb3aaff
michaelphan26 <phantuanan26@gmail.com>
2021-08-08 21:15:29 +0700

    Fix

--- index.js
+++ index.js
@@ -83,4 +83,4 @@ mongoose
   });
 
 const port = process.env.port || 5000;
-app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
